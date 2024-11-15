aa3797675b2fb9f5b54ac8b3b3f3a50b1375dd32
Kassim Zaid <81573513+kzaid0767@users.noreply.github.com>
2024-11-15 16:33:59 -0500

    work continues

---
+++ src/Intro.jsx
@@ -0,0 +1,9 @@
+export default function Intro(){
+    return(
+        <main className="introPage">
+            <h1>Quizzical</h1>
+            <p>A quiz that tests you on various topics</p>
+            <button className="introBtn">Start Quiz</button>
+        </main>
+    )
+}
